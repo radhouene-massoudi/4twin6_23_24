d6b30b9ddb3080fa9efecb003d03b0b09bcd3ae1
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-09-19 11:10:49 +0100

    seance 1+2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { FormsModule } from '@angular/forms';
+import { ListusersComponent } from './listusers/listusers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    ListusersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
